f27622125196bc625bd12cf2c2f9da8bff90c5d4
mikl778890@yandex.ru <adminadmin123>
2019-03-16 21:34:35 +0300

    cделал для фотолаба

--- server/server.js
+++ server/server.js
@@ -1,33 +1,34 @@
 const
     express = require('express'),
     cors = require('cors'),
-    fr = require('face-recognition'),
-    sleep = require('sleep'),
     request = require('request'),
+    fr = require('face-recognition'),
     http = require('http'),
+    faceHelper = require('./helpers/face'),
     crypto = require('crypto'),
     fs = require('fs'),
-    {createCanvas, loadImage} = require('canvas'),
+    {canvas} = require('canvas'),
     app = express();
+
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     http.get(req.query.url, async (response) => {
-    
-        const fileName = crypto.randomBytes(18).toString('hex') + ".png";
+        
+        const fileName = crypto.randomBytes(18).toString('hex') + ".jpg";
         const uploadFolder = __dirname + "/../upload/";
         const pathFoFile = uploadFolder + fileName;
         const file = fs.createWriteStream(pathFoFile);
-    
+        
         /** После того как файл сохранился на сервер **/
-        response.pipe(file).on('finish', () => {
-            
-            const faceImage = fr.loadImage(pathFoFile);
-            const detector = fr.FaceDetector()
-            const faceImages = detector.locateFaces(faceImage);
-            console.log(faceImages);
-            res.json(fileName);
+        response.pipe(file).on('finish', async () => {
+            const faceImg = fr.loadImage(pathFoFile);
+            const predictor = fr.FaceLandmark68Predictor();
+            const detector = new fr.FrontalFaceDetector();
+            const faceRect = detector.detect(faceImg).pop();
+            let facePoints = predictor.predict(faceImg, faceRect).getParts();
+            res.json(faceHelper.foramt(facePoints))
         });
     });
 });
