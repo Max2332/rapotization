9924b4d9369eeaa2ad0646496c5cf52abcbc8cdd
mikl778890@yandex.ru <adminadmin123>
2019-03-16 18:44:56 +0300

    cделал для фотолаба

--- server/server.js
+++ server/server.js
@@ -8,13 +8,28 @@ const
     crypto = require('crypto'),
     fs = require('fs'),
     {createCanvas, loadImage} = require('canvas'),
-    ffmepgHelper = require('./helpers/ffmpeg'),
     app = express();
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    const image2 = fr.loadImage('path/to/image2.jpg')
+    http.get(req.query.url, async (response) => {
+    
+        const fileName = crypto.randomBytes(18).toString('hex') + ".png";
+        const uploadFolder = __dirname + "/../upload/";
+        const pathFoFile = uploadFolder + fileName;
+        const file = fs.createWriteStream(pathFoFile);
+    
+        /** После того как файл сохранился на сервер **/
+        response.pipe(file).on('finish', () => {
+            
+            const faceImage = fr.loadImage(pathFoFile);
+            const detector = fr.FaceDetector()
+            const faceImages = detector.locateFaces(faceImage);
+            console.log(faceImages);
+            res.json(fileName);
+        });
+    });
 });
 
 app.listen(3000, () => console.log('server running on port 3000'));
